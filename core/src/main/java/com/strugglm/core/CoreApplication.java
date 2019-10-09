27d131f3d2694e3e3241087ae1a5ea765158589c
strugglm <strugglm@163.com>
2019-10-09 09:41:40 +0800

    初始化

---
+++ core/src/main/java/com/strugglm/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package com.strugglm.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author strugglm
+ */
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
